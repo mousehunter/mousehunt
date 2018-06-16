fef54a1609d3a63745d9c0d887b49d8337c9159c
mousehunter <39212468+mousehunter@users.noreply.github.com>
2018-06-16 23:26:10 +0800

    fix case problem

--- search-mouse.user.js
+++ search-mouse.user.js
@@ -2,7 +2,7 @@
 // @name         Search Mouse
 // @description  Find best location and cheese to catch a mouse
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @author       You
 // @match        https://www.mousehuntgame.com/*
 // @grant        none
@@ -16,7 +16,7 @@
   // Create window function
   window.findMouse = function (nameOfMouse) {
       return mouseDataSrc
-          .filter(data => data.Mouse.toLowerCase() === nameOfMouse)
+          .filter(data => data.Mouse.toLowerCase() === nameOfMouse.toLowerCase())
           .sort((a, b) => {
               const key = 'Attraction Rate'
               return parseInt(a[key], 10) > parseInt(b[key], 10) ? -1 : 1

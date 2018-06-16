a309c4a6ddc9cc6242e45cbc3ac0246e237e18d5
mousehunter <39212468+mousehunter@users.noreply.github.com>
2018-06-16 23:23:58 +0800

    Initial

---
+++ search-mouse.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Search Mouse
+// @description  Find best location and cheese to catch a mouse
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       You
+// @match        https://www.mousehuntgame.com/*
+// @grant        none
+// ==/UserScript==
+
+(async function () {
+  'use strict'
+
+  const mouseDataSrc = await (await fetch('https://raw.githubusercontent.com/mousehunter/mousehunt/master/mouse.json')).json()
+
+  // Create window function
+  window.findMouse = function (nameOfMouse) {
+      return mouseDataSrc
+          .filter(data => data.Mouse.toLowerCase() === nameOfMouse)
+          .sort((a, b) => {
+              const key = 'Attraction Rate'
+              return parseInt(a[key], 10) > parseInt(b[key], 10) ? -1 : 1
+          })
+          .slice(0, 3)
+  }
+})()
